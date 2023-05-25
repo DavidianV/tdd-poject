ca988821cd7e97c82979462481a995d030e4c895
root <root@DESKTOP-ELPBQ96>
2023-05-25 18:32:42 -0400

    number-fun

--- problems/number-fun.js
+++ problems/number-fun.js
@@ -1,9 +1,14 @@
 function returnsThree() {
   // Your code here
+  return 3
 }
 
 function reciprocal(n) {
   // Your code here
+  if (n > 1000000 || n < 1) {
+    throw new RangeError('This is a range error');
+  }
+  return 1 / n;
 }
 
 module.exports = {
--- test/number-fun-spec.js
+++ test/number-fun-spec.js
@@ -1,1 +1,26 @@
 // Your code here
+const {expect} = require('chai');
+const {
+    returnsThree,
+    reciprocal
+    } = require('../problems/number-fun');
+
+    describe('returnsThree', () => {
+        it('Should return the number 3', () => {
+            expect(returnsThree()).to.equal(3);
+        })
+
+    })
+
+    describe('reciprocal(num)', () => {
+        it('Should return the reciprocal of a number', () => {
+            expect(reciprocal(2)).to.equal(.5);
+            expect(reciprocal(10)).to.equal(.1);
+            expect(reciprocal(5)).to.equal(.2);
+        })
+
+        it('Should throw a RangeError if n is out of range', () => {
+            expect(() => reciprocal(-2)).to.throw(RangeError);
+            expect(() => reciprocal(1000001)).to.throw(RangeError);
+        })
+    })

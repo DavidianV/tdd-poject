ef7749715fbcfc228266301a743fbb8185aeae11
root <root@DESKTOP-ELPBQ96>
2023-05-24 20:44:16 -0400

    reverse-string

--- test/reverse-string-spec.js
+++ test/reverse-string-spec.js
@@ -1,1 +1,13 @@
 // Your code here
+const {expect} = require('chai');
+const reverseString = require('../problems/reverse-string')
+
+describe ('reverseString', function() {
+    it('Should return reversed word', function() {
+        expect(reverseString('fun')).to.equal('nuf');
+    })
+
+    it('Should throw TypeError if not a string', function() {
+        expect(() => reverseString(42)).to.throw(TypeError);
+    })
+})
